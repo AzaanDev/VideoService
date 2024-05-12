417671cf2fbcbc5bf1a05dedb288203e1444a372
AzaanDev <xxx.azaan.ali@gmail.com>
2024-05-12 18:30:28 -0400

    Video file upload

---
+++ main.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type VideoTitleResponse struct {
+    Host   string   `json:"host"`
+    Port   int      `json:"port"`
+	Titles []string `json:"titles"`
+}
+
+type Video struct {
+    Title string `json:"title"`
+    Path  string `json:"path"`
+}
+
+type VideoRequest struct {
+	Title string `json:"title"`
+}
+
+type VideoResponse struct {
+	URL string `json:"url"`
+}
+
+func InitDB() *sql.DB {
+    db, err := sql.Open("sqlite3", "videos.db")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    _, err = db.Exec(`CREATE TABLE IF NOT EXISTS videos (title TEXT, path TEXT)`)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = filepath.Walk("videos", func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+
+        if filepath.Ext(path) == ".m3u8" {
+            // Remove the .m3u8 extension from the file name for the title
+            title := strings.TrimSuffix(info.Name(), ".m3u8")
+            // Convert path to use forward slashes
+            normalizedPath := filepath.ToSlash(path)
+
+            if !FileExistsByTitle(db, title) {
+                _, err := db.Exec("INSERT INTO videos (title, path) VALUES (?, ?)", title, normalizedPath)
+                if err != nil {
+                    log.Printf("Error inserting data: %v", err)
+                } else {
+                    log.Printf("Added %s to database", title)
+                }
+            } else {
+                log.Printf("%s is already in the database", title)
+            }
+        }
+        return nil
+    })
+
+    if err != nil {
+        log.Fatalf("Error walking through video directory: %v", err)
+    }
+
+    return db
+}
+
+func FileExistsByTitle(db *sql.DB, title string) bool {
+    var exists bool
+    err := db.QueryRow("SELECT EXISTS(SELECT 1 FROM videos WHERE title = ?)", title).Scan(&exists)
+    if err != nil {
+        log.Fatalf("Error checking if title exists: %v", err)
+    }
+    return exists
+}
+
+
+func videoLinkHandler(db *sql.DB) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        if r.Method != http.MethodPost {
+            http.Error(w, "Method not supported", http.StatusMethodNotAllowed)
+            return
+        }
+
+        var req VideoRequest
+        err := json.NewDecoder(r.Body).Decode(&req)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusBadRequest)
+            return
+        }
+        if req.Title == "" {
+            http.Error(w, "Missing title in request body", http.StatusBadRequest)
+            return
+        }
+
+        var videoPath string
+        err = db.QueryRow("SELECT path FROM videos WHERE title = ?", req.Title).Scan(&videoPath)
+        if err != nil {
+            if err == sql.ErrNoRows {
+                http.Error(w, "Video not found", http.StatusNotFound)
+                return
+            } else {
+                http.Error(w, "Database error", http.StatusInternalServerError)
+                return
+            }
+        }
+
+        videoURL := fmt.Sprintf("http://%s/%s", r.Host, videoPath)
+
+        resp := VideoResponse{URL: videoURL}
+        w.Header().Set("Content-Type", "application/json")
+        json.NewEncoder(w).Encode(resp)
+    }
+}
+
+
+func getAllVideosHandler(db *sql.DB, host string, port int) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        if r.Method != http.MethodGet {
+            http.Error(w, "Method not supported", http.StatusMethodNotAllowed)
+            return
+        }
+
+        rows, err := db.Query("SELECT title FROM videos")
+        if err != nil {
+            http.Error(w, "Database query error", http.StatusInternalServerError)
+            return
+        }
+        defer rows.Close()
+
+        var titles []string
+        for rows.Next() {
+            var title string
+            if err := rows.Scan(&title); err != nil {
+                http.Error(w, "Error scanning video data", http.StatusInternalServerError)
+                return
+            }
+            titles = append(titles, title)
+        }
+
+        response := VideoTitleResponse{
+            Titles: titles,
+            Host:   host,
+            Port:   port,
+        }
+
+        w.Header().Set("Content-Type", "application/json")
+        json.NewEncoder(w).Encode(response)
+    }
+}
+
+
+func addHeaders(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		h.ServeHTTP(w, r)
+	}
+}
+
+func main() {
+	db := InitDB()
+    defer db.Close()
+
+	const dir = "videos"
+    const port = 8081
+    const host = "localhost"
+
+	http.Handle("/", addHeaders(http.FileServer(http.Dir(dir))))
+	http.HandleFunc("/video", videoLinkHandler(db))
+	http.HandleFunc("/videos", getAllVideosHandler(db, host, port)) 
+
+	fmt.Printf("Starting server on %v\n", port)
+	log.Printf("Serving %s on HTTP port: %v\n", dir, port)
+	
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
